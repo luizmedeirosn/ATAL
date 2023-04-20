b6b40617d9c02cd703d99d4f03f7e3c0ed7e1245
Neto Medeiros <netomedeiros1500@gmail.com>
2023-04-20 02:42:34 -0300

    Fix: Updated file name

--- src/GreedyAlgorithm_LeastNumberOfStops.java
+++ src/GreedyAlgorithm_LeastNumberOfStops.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class GreedySearch_LeastNumberOfStops {
+public class GreedyAlgorithm_LeastNumberOfStops {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
